3dd5900857e4f5c69fc7c9565199ac2faf0ac6d8
Wiso86 <wiso86@gmail.com>
2016-12-27 23:15:18 +0100

    Class 146 - Setup track throttles: Added a TankTrack class for managing the tank movement. Added the input for movement of the tracks, and wired it to its handler.

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *GetName(), Throttle)
+
+	// TODO Clamp actual throttle value so player can't over-drive
+}
